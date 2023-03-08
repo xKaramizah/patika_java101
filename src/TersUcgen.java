62840fa5a804589e38d2262934dc16d5767a42b2
Emre CAN <i.e.can@yandex.com>
2023-03-08 15:16:23 +0300

    Ters Üçgen

---
+++ src/TersUcgen.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//Java ile basamak sayısının kullanıcıdan alınan ve döngüler kullanılarak, yıldızlar(*) ile ekrana ters üçgen çizen programı yazın.
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Basamak sayısı giriniz: ");
+        int n = in.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int k = 1; k <= (n - i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
