cc4bac3ab021860405c36c74754b6722ee1d8e58
Emre CAN <i.e.can@yandex.com>
2023-03-11 19:19:50 +0300

    Duplicate odevi update

--- src/OdevDuplicate.java
+++ src/OdevDuplicate.java
@@ -8,14 +8,18 @@ public class OdevDuplicate {
         return false;
     }
 
+    static boolean isEven(int value) {
+        return value % 2 == 0;
+    }
+
     public static void main(String[] args) {
-        int[] list = {1, 3, 3, 7, 2, 7, 6, 9, 99, 9, 33, 33, 51, 75, 52, 45, 54, 74, 51, 45};
+        int[] list = {1, 3, 3, 7, 2, 7, 6, 9, 99, 6, 33, 33, 51, 75, 52, 45, 54, 74, 54, 45, 2};
         int[] dList = new int[list.length];
 
         for (int i = 0; i < list.length; i++) {
             for (int j = 1; j < list.length; j++) {
                 if (j != i && (list[i] == list[j])) {
-                    if (!isMet(dList, list[i])){
+                    if (!isMet(dList, list[i]) && isEven(list[i])) {
                         dList[i] = list[i];
                     }
                     break;
@@ -24,8 +28,8 @@ public class OdevDuplicate {
         }
         System.out.print("[ ");
         for (int i : dList) {
-            if (i != 0){
-                System.out.print( i + " ");
+            if (i != 0) {
+                System.out.print(i + " ");
             }
         }
         System.out.print("]");
