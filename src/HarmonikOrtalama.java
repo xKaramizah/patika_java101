0945d7633e199fa8e41eabfe78defd1470afbcfb
Emre CAN <i.e.can@yandex.com>
2023-03-11 16:24:59 +0300

    Harmonik Ortalama hesaplanması eklendi

---
+++ src/HarmonikOrtalama.java
@@ -0,0 +1,24 @@
+import java.awt.desktop.AboutEvent;
+import java.util.Scanner;
+
+public class HarmonikOrtalama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Eleman sayısı : ");
+        int n = input.nextInt();
+        double harmonikSeri = 0;
+        double harmonikOrt = 0;
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print((i + 1) + ". sayıyı giriniz: ");
+            array[i] = input.nextInt();
+            harmonikSeri += (double) 1 / array[i];
+        }
+        harmonikOrt = array.length / harmonikSeri;
+
+        System.out.println("Girilen " + n + " sayının : \n" +
+                "Harmonik serisi : " + harmonikSeri +
+                "\nHarmonik Ortalaması : " + harmonikOrt);
+    }
+}
